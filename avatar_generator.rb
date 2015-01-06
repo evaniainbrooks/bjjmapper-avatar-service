57218c9b7eb5c88c26575aff6869eb26fbb813df
Evan <evan@bjjmapper.com>
2015-01-05 22:45:14 -0800

    Formatting

--- avatar_generator.rb
+++ avatar_generator.rb
@@ -12,7 +12,7 @@ class AvatarGenerator
     img_text = initials_for(options[:name])
     color = ColorGenerator.color_for(options[:name])
     img = Image.new(IMAGE_WIDTH, IMAGE_HEIGHT) {
-        self.background_color = 'transparent' 
+      self.background_color = 'transparent'
     }
 
     circle_draw = Draw.new {
@@ -21,7 +21,6 @@ class AvatarGenerator
 
     circle_draw.circle(IMAGE_WIDTH / 2, IMAGE_HEIGHT / 2, 20, 20)
     circle_draw.draw(img)
-    
     font_draw = Draw.new {
       self.font_family ='helvetica'
       self.pointsize = POINT_SIZE
@@ -35,7 +34,7 @@ class AvatarGenerator
     img.format = options[:format]
     img.to_blob
   end
-  
+
   def self.initials_for(name)
     name.split(/\W/).take(3).map{|tok| tok[0].upcase }.join('')
   end
